75e3ff27075c3ade31a0f29a38fc878242252c4f
dileep <yarramanenidileep@gmail.com>
2025-12-12 15:25:49 +0530

    Fix login URLs to point to Render backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,27 @@
 import './Header.css';
 
 function Header({ user }) {
-  return (
-    <header className="header">
-      <div className="header-content">
-        <div className="logo">
-          <span className="logo-icon">🎂</span>
-          <span className="logo-text">Birthday Reminder</span>
-        </div>
-        <nav className="nav">
-          {user ? (
-            <div className="user-menu">
-              <img src={user.image} alt={user.displayName} className="user-avatar" />
-              <span>{user.displayName}</span>
-              <a href="/auth/logout" className="btn-logout">Logout</a>
+    return (
+        <header className="header">
+            <div className="header-content">
+                <div className="logo">
+                    <span className="logo-icon">🎂</span>
+                    <span className="logo-text">Birthday Reminder</span>
+                </div>
+                <nav className="nav">
+                    {user ? (
+                        <div className="user-menu">
+                            <img src={user.image} alt={user.displayName} className="user-avatar" />
+                            <span>{user.displayName}</span>
+                            <a href="https://birthday-remainder-zodg.onrender.com/auth/logout" className="btn-logout">Logout</a>
+                        </div>
+                    ) : (
+                        <a href="https://birthday-remainder-zodg.onrender.com/auth/google" className="btn-login">Login with Google</a>
+                    )}
+                </nav>
             </div>
-          ) : (
-            <a href="/auth/google" className="btn-login">Login with Google</a>
-          )}
-        </nav>
-      </div>
-    </header>
-  );
+        </header>
+    );
 }
 
 export default Header;
